113395de900085b8707b6a3f0f70f3834b19288e
kobekeo24 <kobekeo@gmail.com>
2022-10-22 12:42:38 -0400

    End of day push Uart TX

--- UART_TX_Example/UART_TX/UART_TX.srcs/sources_1/new/UART_tx_top.vhd
+++ UART_TX_Example/UART_TX/UART_TX.srcs/sources_1/new/UART_tx_top.vhd
@@ -40,18 +40,20 @@ end UART_tx_top;
 
 architecture Behavioral of UART_tx_top is
 
-type state_type is (TX_WAIT_BTN,TX_SEND_CHAR,TX_LOAD_NEXT_CHAR,TX_SEND_WAIT);
+type state_type is (TX_WAIT_BTN,TX_SEND_CHAR,TX_SEND_WAIT);
 
 signal state : state_type := TX_WAIT_BTN;
+signal index : integer;
+signal count : integer := 5;
 signal uartRdy: std_logic;
-signal uartSend:std_logic;
+signal uartRdyPrev  : std_logic;
+signal uartSend     : std_logic;
+signal uartSendPrev : std_logic;
 signal uartData:std_logic_vector(7 downto 0);
 signal initStr: std_logic_vector(7 downto 0);
 signal btnCclr: std_logic;
 signal btnC_prev: std_logic;
 signal helloStr: std_logic_vector(39 downto 0) := x"4F4C4C4548";
-signal StrIndex: integer := 0;
-signal wait_timer: integer := 0;
 
 component UART_tx_ctrl is
 generic(baud : integer);
@@ -70,41 +72,35 @@ port(btn   : in std_logic;
 end component debounce;
 
 begin
-
+index <= count;
 process(clk)
 begin
 if rising_edge(clk) then
+uartRdyPrev <= uartRdy;
+uartSendPrev <= uartSend;
 btnC_prev <= btnCclr;
 case state is
 when TX_WAIT_BTN =>
-    --if btnC_prev = '0' and btnCclr = '1' then
-        initStr <=  helloStr((StrIndex+7) downto StrIndex);
+    if btnC_prev = '0' and btnCclr = '1' then
         state <= TX_SEND_CHAR;
-    --end if;
+    end if;
 when TX_SEND_CHAR =>
-    if(StrIndex < 40) then
-        uartData <= initStr;
-        initStr <=  helloStr((StrIndex+7) downto StrIndex);
-        StrIndex <= StrIndex + 8; 
-         state <= TX_LOAD_NEXT_CHAR;
-    else
-        state <= TX_SEND_WAIT;
-    end if;
-   
-when TX_LOAD_NEXT_CHAR =>
-    if uartRdy = '1' then  
-        state <= TX_SEND_CHAR;
-    end if;
+    uartData <= helloStr(index*8-1 downto (index-1)*8);
+    state <= TX_SEND_WAIT;
+    
 when TX_SEND_WAIT =>
-    if uartRdy = '1' then   
-        StrIndex <= 0; 
+    if uartRdy = '1' then
+        if index = 0  then 
+            index <= 5;
+        else
+            index <= index - 1;
+        end if;
         state <= TX_WAIT_BTN;
     end if;
 end case;
 end if;
 end process;
 
---StrIndex <= 0 when(StrIndex >= 32 and state = TX_SEND_WAIT) else StrIndex+8;
 uartSend <= '1' when (state = TX_SEND_CHAR) else '0';
 TX : UART_tx_ctrl generic map(baud => 19200)
 port map(send => uartSend, data => uartData, clk => clk, ready => uartRdy, uart_tx => RsTx);
